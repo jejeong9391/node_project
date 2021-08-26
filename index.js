c9a7110b5ecd61dcb9e4df6a0a0742f003525ad4
jejeong9391 <wjdwldms9391@gmail.com>
2021-08-26 19:50:09 +0900

    TIL_node 2021-08-26

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const colors = require('colors'); // 터미널에 다양한 색으로 문자열을 표현할 수 있는 모듈 ㅎ.ㅎ
+const colors = require('colors') // 터미널에 다양한 색으로 문자열을 표현할 수 있는 모듈 ㅎ.ㅎ
 
-const s1 = "Luce sicut stellae";
-const s2 = "Opportunities are never lost";
-const s3 = "The other fellow takes those you miss.";
+const s1 = "Luce sicut stellae"
+const s2 = "Opportunities are never lost"
+const s3 = "The other fellow takes thoseyou miss.";
 
-console.log(s1.underline.rainbow);
-console.log(s2.inverse.blue);
-console.log(s3.underline.yellow);
+console.log(s1.underline.rainbow)
+//console.log(s2.inverse.blue)
+//console.log(s3.underline.yellow)
+
+
+//2021-08-26 익명함수 사용하기 (일반함수, 화살표함수 = 동일처리)
+const f1= function(s){ console.log(s2.inverse.blue)} 
+const f2= (s)=>{console.log(s3.underline.yellow)}
+f1('a')
+f2('b')
+
+// 익명함수를 변수의 매개변수로 사용하기
---
+++ today_i_learned/20210826_async.js
@@ -0,0 +1,14 @@
+/*2021-08-26 1-7(P.63)
+1. 동기 비동기 차이 
+2. 익명함수, 화살표함수
+3. 프로미스와 제너레이터 
+4. async/awit를 이용한 비동기 처리 
+*/
+
+//익명함수 사용하기 (일반함수, 화살표함수 = 동일처리)
+const f1= function(s){ console.log(s2.inverse.blue)} 
+const f2= (s)=>{console.log(s3.underline.yellow)}
+f1('nomal function')
+f2('arrow fuction')
+
+// 익명함수를 변수의 매개변수로 사용하기
